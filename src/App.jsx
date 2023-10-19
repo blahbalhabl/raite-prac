b9f4efbef6e0919c77157b5ef0b842aa5928091f
Kerby Sarcia <kosarcia@rpinnotech.com>
2023-10-19 11:57:39 +0800

    dashboard navs

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import HomePage from './pages/HomePage';
-import Dashboard from './pages/Dashboard';
-import Header from './components/Header';
-import LoginPage from './pages/LoginPage';
-import RequireAuth from './components/RequireAuth';
-import SIgnupPage from './pages/SIgnupPage';
-import NotFound from './pages/NotFound';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import LoginPage from "./pages/LoginPage";
+import RequireAuth from "./components/RequireAuth";
+import SIgnupPage from "./pages/SIgnupPage";
+import NotFound from "./pages/NotFound";
+import Appointment from "./pages/Appointment";
+import Doctors from "./pages/Doctors";
+import History from "./pages/History";
+import Hospitals from "./pages/Hospitals";
+import Prescriptions from "./pages/Prescriptions";
 
 const App = () => {
   return (
-    <div className='h-screen w-screen'>
+    <div className="h-screen w-screen">
       <Router>
         <AuthProvider>
           <Header />
           <Routes>
-            <Route path='/' element={<HomePage />}/>
-            <Route path='/auth/login' element={<LoginPage />}/>
-            <Route path='/auth/signup' element={<SIgnupPage />}/>
-            <Route path='*' element={<NotFound />}/>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/auth/login" element={<LoginPage />} />
+            <Route path="/auth/signup" element={<SIgnupPage />} />
+            <Route path="*" element={<NotFound />} />
             <Route element={<RequireAuth />}>
-              <Route path='/dashboard' element={<Dashboard />}/>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/appointment" element={<Appointment />} />
+              <Route path="/doctors" element={<Doctors />} />
+              <Route path="/history" element={<History />} />
+              <Route path="/hospitals" element={<Hospitals />} />
+              <Route path="/prescriptions" element={<Prescriptions />} />
             </Route>
           </Routes>
         </AuthProvider>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,64 @@
+/* eslint-disable react/prop-types */
+import { Icon } from "@iconify/react";
+import { useNavigate } from "react-router-dom";
 
+const Card = ({ icon, title, description, link }) => {
+  const nav = useNavigate();
+  return (
+    <div
+      onClick={() => nav(link)}
+      className="shadow-lg border min-h-[200px] group hover:bg-blue-500 transition md:w-[300px] rounded-md flex flex-col justify-center items-center p-1 gap-1 cursor-pointer"
+    >
+      <Icon
+        className="text-blue-500 text-[50px] group-hover:text-white"
+        icon={icon}
+      />
+      <h1 className="font-bold text-lg group-hover:text-white">{title}</h1>
+      <p className="text-sm group-hover:text-white">{description}</p>
+    </div>
+  );
+};
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="flex justify-center">
+      <div className="p-5 w-[80%]">
+        <h1 className="font-bold text-xl">Dashboard</h1>
+        <div className="py-5 flex flex-wrap md:flex-row flex-col gap-5">
+          <Card
+            link={"/appointment"}
+            icon={"ic:round-calendar-month"}
+            title={"Book an Appointment"}
+            description={"Easily connect with your doctors"}
+          />
+          <Card
+            link={"/prescriptions"}
+            icon={"tabler:prescription"}
+            title={"Prescriptions"}
+            description={"View your prescriptions from your doctors"}
+          />
+          <Card
+            link={"/history"}
+            icon={"tabler:history"}
+            title={"History"}
+            description={"View your medical history"}
+          />
+          <Card
+            link={"/doctors"}
+            icon={"ep:avatar"}
+            title={"Doctors"}
+            description={"View your doctors"}
+          />
+          <Card
+            link={"/hospitals"}
+            icon={"solar:hospital-bold"}
+            title={"Hospitals"}
+            description={"View your hospitals"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
